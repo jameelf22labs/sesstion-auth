186fa3530ef25f99076438b93245e6dff539d661
jameelf22labs <jameel.m@f22labs.com>
2025-07-17 17:11:44 +0530

    add auth controller

--- src/controller/auth-controller.ts
+++ src/controller/auth-controller.ts
@@ -1,2 +1,40 @@
+import { NextFunction, Request, Response } from "express";
+import { loginProvider, signUpProvider } from "../service";
+import { LoginCredantialDto, SignupDto } from "../dtos";
 
+export const loginHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const providerResponse = await loginProvider(
+      request.body as LoginCredantialDto
+    );
+  } catch (error) {
+    next(error);
+  }
+};
 
+export const signUpHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const providerResponse = await signUpProvider(request.body as SignupDto);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logoutHandler = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
