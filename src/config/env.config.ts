af52d51da1ed5065c47fbbd0d75cb7e428088341
jameelf22labs <jameel.m@f22labs.com>
2025-07-17 13:27:18 +0530

    setup sequalize pg

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -2,6 +2,12 @@ type EnvConfig = {
   Port: number;
   RedisUrl: string;
   SessionSecret: string;
+  Postgres: {
+    host: number;
+    port: number;
+    user: string;
+    pass: string;
+  };
 };
 
 const env: EnvConfig = {
@@ -9,6 +15,13 @@ const env: EnvConfig = {
   RedisUrl: process.env.REDIS_URL || "redis://localhost:6379",
   SessionSecret:
     process.env.SESSION_SECRET || "hello__________it_________secured",
+
+  Postgres: {
+    host: Number(process.env.POSTGRESS_HOST),
+    port: Number(process.env.POSTGRESS_PORT),
+    user: process.env.POSTGRESS_USERNAME || "root",
+    pass: process.env.POSTGRESS_PASSWORD || "root",
+  },
 };
 
 export default env;
---
+++ src/config/sequelize-config.ts
@@ -0,0 +1,16 @@
+import { Sequelize } from "@sequelize/core";
+import { PostgresDialect } from "@sequelize/postgres";
+import env from "./env.config";
+
+const sequelize = new Sequelize({
+  dialect: PostgresDialect,
+  database: "api_book",
+  user: env.Postgres.user,
+  password: env.Postgres.pass,
+  host: env.Postgres.user,
+  port: env.Postgres.port,
+  clientMinMessages: "notice",
+  models: [],
+});
+
+export default sequelize;
